cb8e4b1240c4ef31102335ca37e442c146d8f5dc
amillaly <leydif840@gmail.com>
2025-03-19 14:49:52 -0600

    Se anadieron algunos colores al boton de Buscar y al buscador

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
 import { Form, FormControl, Button } from "react-bootstrap";
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { InputGroup } from "react-bootstrap";
+
 
 const SearchBar = ({ onSearch }) => {
   const [query, setQuery] = useState("");
@@ -13,14 +17,25 @@ const SearchBar = ({ onSearch }) => {
 
   return (
     <Form className="d-flex my-3" onSubmit={handleSearch}>
+      <InputGroup className="me-2">
+        <InputGroup.Text>
+          <i className="bi bi-search"></i>
+        </InputGroup.Text>
       <FormControl
         type="text"
         placeholder="Buscar película..."
         className="me-2"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        style={{
+          backgroundColor: '#f9d3a1',
+          borderColor: '#f5a623',
+          padding: '5px',
+          borderRadius: '4px',
+        }}
       />
-      <Button variant="primary" type="submit">
+      </InputGroup>
+      <Button variant="primary" type="submit" style={{ backgroundColor: '#f5a623', borderColor: '#f5a623' }}>
         Buscar
       </Button>
     </Form>
