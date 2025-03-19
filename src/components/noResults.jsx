75f53d0d8ed8d28a1f07a054b6d6aa3ae300f303
amillaly <leydif840@gmail.com>
2025-03-19 17:12:40 -0600

    Insercion de la imagen con animacion en noResult

--- src/components/noResults.jsx
+++ src/components/noResults.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 import { Image } from "react-bootstrap";
-import noResultsImg from "../assets/cine.png";
+import noResultsImg from "../assets/crying-cat.webp";
 
 const NoResults = () => {
+  const catStyle = {
+    width: '150px',
+    animation: 'bounce 1.5s infinite', 
+  };
+
+  const keyframesStyle = `
+    @keyframes bounce {
+      0%, 100% {
+        transform: translateY(0);
+      }
+      50% {
+        transform: translateY(-20px);
+      }
+    }
+  `;
+
   return (
     <div className="text-center my-4">
-      <Image src={noResultsImg} alt="No hay resultados" fluid width={300} />
+      <style>{keyframesStyle}</style>
+      <Image src={noResultsImg} alt="No hay resultados" fluid width={300} style={catStyle} />
       <h5 className="mt-3 text-muted">Ups....No se han encontrado resultados </h5>
     </div>
   );
