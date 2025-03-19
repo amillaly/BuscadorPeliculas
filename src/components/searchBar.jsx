4b8b738d4e98c15a7736d16a54a20f198d272fdc
dazer-m-l <dazer.mc1397@gmail.com>
2025-03-19 17:47:41 -0600

    Refactor searchBar.jsx Busqueda RealTime

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from "react";
-import { Form, FormControl, Button } from "react-bootstrap";
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { InputGroup } from "react-bootstrap";
-
+import { Form, FormControl, Button, InputGroup } from "react-bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const SearchBar = ({ onSearch }) => {
   const [query, setQuery] = useState("");
@@ -15,25 +13,32 @@ const SearchBar = ({ onSearch }) => {
     }
   };
 
+  const handleKeyUp = () => {
+    if (query.trim() !== "") {
+      onSearch(query);
+    }
+  };
+
   return (
     <Form className="d-flex my-3" onSubmit={handleSearch}>
       <InputGroup className="me-2">
         <InputGroup.Text className="bg-warning border-warning">
           <i className="bi bi-search"></i>
         </InputGroup.Text>
-      <FormControl
-        type="text"
-        placeholder="Buscar película..."
-        className="me-2"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        style={{
-          backgroundColor: '#f9d3a1',
-          borderColor: '#f5a623',
-          padding: '5px',
-          borderRadius: '4px',
-        }}
-      />
+        <FormControl
+          type="text"
+          placeholder="Buscar película, actor o género..."
+          className="me-2"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyUp={handleKeyUp}
+          style={{
+            backgroundColor: "#f9d3a1",
+            borderColor: "#f5a623",
+            padding: "5px",
+            borderRadius: "4px",
+          }}
+        />
       </InputGroup>
       <Button variant="warning" type="submit">
         Buscar
