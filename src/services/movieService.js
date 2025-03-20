efa9c393b7a668e6d8b77e6f585c40dded6c75bd
Kevin <kevinbadillod@gmail.com>
2025-03-19 18:06:40 -0600

    [add]:se anadieron cambios a los componentes

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -6,3 +6,18 @@ export const searchMovies = async (query) => {
   });
   return response.data.results;
 };
+
+
+//Agregar funcion para el apartado de reparto de los actores  
+
+export const getMovieCredits = async (movieId) => {
+  try {
+    const response = await axiosInstance.get(`/movie/${movieId}/credits`);
+    return response.data;    
+  } catch (error) {
+    console.error('Error al obtener créditos:', error);
+    throw error;
+  }
+};
+
+//Agregar funcion para el apartado de reparto de los actores
